dc9de677690dbc21692f186bc45dce28d4be6eaa
J.P. Given <jgiven@gmail.com>
2022-11-29 12:57:51 -0800

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "welcome/index"
 
   get "component/:name" => "component#name", :as => :component_default
-  get "component/:name/expanded" => "component#expanded", :as => :component_expanded
 
   root "welcome#index"
 end
