7be317fc4b504a67df1fd364b12398a999cceb81
J.P. Given <jgiven@gmail.com>
2022-11-29 12:54:25 -0800

    Contrived test

--- test/components/example_component_test.rb
+++ test/components/example_component_test.rb
@@ -4,9 +4,9 @@ require "test_helper"
 
 class ExampleComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ExampleComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+    assert_equal(
+      %(<h3>Example Component</h3>),
+      render_inline(ExampleComponent.new()).css("h3").to_html
+    )
   end
 end
