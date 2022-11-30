bc0601056b487915aeea170c523455d2631836e7
J.P. Given <jgiven@gmail.com>
2022-11-30 12:31:26 -0800

    Updated expanded route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "welcome/index"
 
   get "component/:name" => "component#name", :as => :component_default
+  get "component/:name/expanded" => "component#name", :as => :component_expanded
 
   root "welcome#index"
 end
