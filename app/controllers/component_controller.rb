67363c9d3fd47207e249753594e65eb2e3fce701
J.P. Given <jgiven@gmail.com>
2022-12-01 11:05:04 -0800

    Comments.

--- app/controllers/component_controller.rb
+++ app/controllers/component_controller.rb
@@ -1,6 +1,6 @@
 class ComponentController < ApplicationController
   include Cachable
-  
+
   before_action :put_time
   caches_action :name, expires_in: default_cache_time
 
@@ -17,6 +17,9 @@ class ComponentController < ApplicationController
 
   private
 
+  # Temporary method to make sure action caching
+  # is working properly. This will be swapped out
+  # for session verification.
   def put_time
     p "+_+_+_+_+_+"
     p Time.now
