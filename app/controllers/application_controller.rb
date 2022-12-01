29f480dd350d7c6e77ffce8a2aa96a99c4bd18b7
J.P. Given <jgiven@gmail.com>
2022-12-01 11:47:07 -0800

    Moving session logic to application controller for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  before_action :set_user_on_session
+
+  private
+
+  def set_user_on_session
+    response = Base.profile_request("#{Rails.application.config.base_profile_service_url}/#{params[:user_uuid]}", {nucleus_view: "partner_extended"})
+
+    user = response[:data][0]
+
+    h = {
+      full_name: user[:full_name],
+      uuid: user[:uuid],
+      mls_number: user[:mls_number],
+      nrds_number: user[:nrds_number],
+      member_status: user[:member_status],
+      subscription_status: user[:subscription_status],
+      member_role: user[:member_role],
+      office_external_id: user[:office][:office_external_id],
+      company_external_id: user[:company][:company_external_id],
+      board_external_id: user[:board][:board_external_id],
+      board_mls_id: user[:board][:mls_id],
+    }
+
+    session[:current_user] = h
+  end
 end
--- app/controllers/component_controller.rb
+++ app/controllers/component_controller.rb
@@ -1,7 +1,6 @@
 class ComponentController < ApplicationController
   include Cachable
-
-  before_action :return_agent
+  
   caches_action :name, expires_in: default_cache_time
 
   def name
@@ -13,38 +12,5 @@ class ComponentController < ApplicationController
     end
 
     render(obj)
-  end
-
-  private
-
-  # Temporary method to make sure action caching
-  # is working properly. This will be swapped out
-  # for session verification.
-  def put_time
-    p "+_+_+_+_+_+"
-    p Time.now
-    p Rails.application.config.base_profile_service_url
-  end
-
-  def return_agent
-    response = Base.profile_request("#{Rails.application.config.base_profile_service_url}/#{params[:user_uuid]}", {nucleus_view: "partner_extended"})
-
-    user = response[:data][0]
-
-    h = {
-      full_name: user[:full_name],
-      uuid: user[:uuid],
-      mls_number: user[:mls_number],
-      nrds_number: user[:nrds_number],
-      member_status: user[:member_status],
-      subscription_status: user[:subscription_status],
-      member_role: user[:member_role],
-      office_external_id: user[:office][:office_external_id],
-      company_external_id: user[:company][:company_external_id],
-      board_external_id: user[:board][:board_external_id],
-      board_mls_id: user[:board][:mls_id],
-    }
-
-    session[:current_user] = h
   end
 end
