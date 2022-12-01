7d1b65770b9b1ca1ecffcc75b7db691efe0a2a4d
J.P. Given <jgiven@gmail.com>
2022-12-01 13:44:58 -0800

    Adding helper methods to return data around user.

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,2 +1,14 @@
 module ComponentHelper
+  def return_user
+    User.get_office(session[:current_user][:uuid])
+  end
+  def return_office
+    User.get_office(session[:current_user][:office_external_id])
+  end
+  def return_company
+    User.get_office(session[:current_user][:company_external_id])
+  end
+  def return_board
+    User.get_office(session[:current_user][:board_external_id])
+  end
 end
