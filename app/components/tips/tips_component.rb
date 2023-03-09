d6b2f805793569ac90016c18fffd7774758fcaf6
Kevin Lee Drum <kevinleedrum@gmail.com>
2023-03-09 14:47:29 -0500

    Always use demo params as fallbacks

--- app/components/tips/tips_component.rb
+++ app/components/tips/tips_component.rb
@@ -7,18 +7,18 @@ class Tips::TipsComponent < ViewComponent::Base
 
   def before_render
     @library_mode ||= params[:library_mode]
-    if @library_mode
-      @tip = "Utilizing online marketing platforms, such as MoxiPromote, can help you reach a wider audience and generate leads."
-      @cta_label = "Learn more"
-      @cta_url = ""
-      @bg = "1"
-    else
-      tips_params = params["tips"]
-      @tip = tips_params["description"]
-      @cta_label = tips_params["cta_label"]
-      @cta_url = tips_params["cta_url"]
-      @bg = tips_params["bg"] || "1"
-    end
+    demo_params = {
+      "description" => "Utilizing online marketing platforms, such as MoxiPromote, can help you reach a wider audience and generate leads.",
+      "cta_label" => "Learn more",
+      "cta_url" => "",
+      "bg" => "1"
+    }
+    tips_params = params["tips"] || demo_params
+    @id = tips_params["id"]
+    @tip = tips_params["description"]
+    @cta_label = tips_params["cta_label"]
+    @cta_url = tips_params["cta_url"]
+    @bg = tips_params["bg"] || "1"
     @widget = Widget.find_by(component: "tips")
   end
 end
