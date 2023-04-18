818cfd23b5ce82be5e3d56108a1116ed81f481bc
Kevin Lee Drum <kevinleedrum@gmail.com>
2023-04-18 09:12:21 -0400

    Add office_uuid to event

--- app/sidekiq/event_logger_job.rb
+++ app/sidekiq/event_logger_job.rb
@@ -1,9 +1,10 @@
 class EventLoggerJob
   include Sidekiq::Job
 
-  def perform(event_type, component, event_data, agent_uuid, company_uuid, board_uuid)
+  def perform(event_type, component, event_data, agent_uuid, company_uuid, board_uuid, office_uuid)
     event_data[:component] = component
     event_data[:board_uuid] = board_uuid
+    event_data[:office_uuid] = office_uuid
 
     @result = WmsSvcConsumer::Models::Event.log("nucleus_logging_event", "nucleus", {
     # TODO: Use widget events instead of nucleus events once they are added to the event service
