fa7e13027e30f9481f3c4be8dda11d4c2ba55007
Kevin Lee Drum <kevinleedrum@gmail.com>
2023-04-25 15:58:39 -0400

    Fix active widget filtering OT-161

--- app/components/widget_panel/widget_panel_component.rb
+++ app/components/widget_panel/widget_panel_component.rb
@@ -8,12 +8,10 @@ class WidgetPanel::WidgetPanelComponent < ViewComponent::Base
     @active_widgets = @active_widgets.filter { |w| w.view_component.present? }
     @user_uuid = session.dig(:current_user, :uuid)
     user_removed_widget_ids = UserWidget.where(user_uuid: @user_uuid, removed: true).pluck(:widget_id)
-    user_restored_widget_ids = UserWidget.where(user_uuid: @user_uuid, removed: false).pluck(:widget_id)
+    visible_widget_ids = @active_widgets.pluck(:id) - user_removed_widget_ids
     @widgets = Widget
       .joins("LEFT OUTER JOIN user_widgets ON user_widgets.widget_id = widgets.id AND user_widgets.user_uuid = '#{@user_uuid}'")
-      .where(id: @active_widgets.pluck(:id))
-      .where.not(id: user_removed_widget_ids)
-      .or(Widget.where(id: user_restored_widget_ids))
+      .where(id: visible_widget_ids)
       .order('user_widgets.row_order ASC NULLS LAST')
   end
 end
