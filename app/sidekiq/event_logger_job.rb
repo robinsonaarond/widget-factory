641679afc19af62ffa3acc849be4b4ffafd1aa38
Kevin Lee Drum <kevinleedrum@gmail.com>
2023-06-13 15:03:39 -0400

    Fix event_data in sidekiq job

--- app/sidekiq/event_logger_job.rb
+++ app/sidekiq/event_logger_job.rb
@@ -2,6 +2,7 @@ class EventLoggerJob
   include Sidekiq::Job
 
   def perform(event_type, component, event_data, agent_uuid, company_uuid, board_uuid, office_uuid)
+    event_data = JSON.parse(event_data) || {}
     event_data[:component] = component
     event_data[:board_uuid] = board_uuid
     event_data[:office_uuid] = office_uuid
